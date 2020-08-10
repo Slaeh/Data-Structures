48f6c05eb63514d2c24f879edc517caa367fb9c2
Slaeh <harjitliyal@gmail.com>
2020-08-10 19:07:52 -0400

    new class: not sure if logic is correct

---
+++ Stack/src/isBalanced.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+public class isBalanced {
+	public boolean Balance(String input) {
+		Stack<Character> stack = new Stack<>();
+		for(int i = 0; i < input.length(); i++) {
+			if(input.charAt(i) == '(' || input.charAt(i) == '<' || input.charAt(i) == '[' || input.charAt(i) == '{') {
+				stack.push(input.charAt(i));
+			}
+			if(input.charAt(i) == ')' || input.charAt(i) == '>' || input.charAt(i) == ']' || input.charAt(i) == '}') {
+				if(stack.isEmpty()) {
+					return false;
+				}else {
+					var symbol = stack.pop();
+					if(symbol =='(' && input.charAt(i) == ')' || 
+					   symbol == '<' && input.charAt(i) == '>'||
+					   symbol == '[' && input.charAt(i) == ']'||
+					   symbol == '{' && input.charAt(i) == '}') {
+						
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+	}
+}
