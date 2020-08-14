c3fc66f7be2e50a1447b5b8b43755d58d13a0dde
Slaeh <harjitliyal@gmail.com>
2020-08-14 17:05:30 -0400

    New class

---
+++ Queue/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) {
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(10);
+		queue.add(20);
+		queue.add(30);
+		System.out.println(queue);
+		reverseQueue(queue);
+		System.out.println(queue);
+	}
+	
+	public static void reverseQueue(Queue<Integer> queue) {
+		if(queue.isEmpty()) {
+			throw new IllegalStateException();
+		}else{
+			Stack<Integer> stack = new Stack<>();
+			while(!queue.isEmpty()) {
+				stack.push(queue.remove());
+			}
+			while(!stack.isEmpty()) {
+				queue.add(stack.pop());
+			}
+		}
+	}
+}
